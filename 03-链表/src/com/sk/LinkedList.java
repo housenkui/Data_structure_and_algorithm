9c62c43ea80d94c8e9349e3b326eba3ca8142dcf
housenkui <housenkui@gmail.com>
2020-05-06 14:17:30 +0800

    完成单向链表、双向链表、单向循环链表、双向循环链表的增、删、改、查.

--- 03-链表/src/com/sk/LinkedList.java
+++ 03-链表/src/com/sk/LinkedList.java
@@ -1,15 +1,39 @@
 package com.sk;
 
-
+/*
+ * 双向链表
+ * */
 public class LinkedList <E> extends AbstractList<E>{
 	 private Node <E> first;
+	 private Node<E>  last;
 	 
 	 private static class Node<E> {
 		 E element;
+		 Node<E> prev;
 		 Node<E> next;
-		 public Node(E element, Node<E> next) {
+
+		 public Node(Node<E> prev,E element, Node<E> next) {
+			this.prev = prev;
 			this.element = element;
 			this.next = next;
+		 }
+		 @Override
+		 public String toString() {
+			
+			StringBuilder sb = new StringBuilder();
+			if (prev != null) {
+				sb.append(prev.element);
+			} else {
+				sb.append("null");
+			}
+			sb.append("_").append(element).append("_");
+			
+			if (next != null) {
+				sb.append(next.element);
+			} else {
+				sb.append("null");
+			}
+			return sb.toString();
 		}
 	 }
 
@@ -18,6 +42,7 @@ public class LinkedList <E> extends AbstractList<E>{
 	public void clear() {
 		size = 0;
 		first = null;
+		last = null;
 	}
 
 	@Override
@@ -35,32 +60,70 @@ public class LinkedList <E> extends AbstractList<E>{
 
 	@Override
 	public void add(int index, E element) {
-		if (index == 0) {
-			first = new Node<E>(element, first);
+		rangeCheckForAdd(index);
+		if (index == size) {//往最后面添加元素
+			Node<E> oldLast  = last;
+			last = new Node<> (oldLast, element, null);
+			if (oldLast == null) {//这里添加的是第一个元素
+				first = last;
+			}else {
+				oldLast.next = last;
+			}
+		}else {
+			Node<E> next = node(index);
+			Node<E> prev = next.prev;
+			Node<E> node = new Node<>(prev, element, next);
+			next.prev = node;
+			
+			if (prev == null) { //index = 0
+				first = node;
+			} else {
+				prev.next = node;
+			}
 		}
-		else {
-			Node<E> prev = node(index - 1);//首先通过index获取当前要添加节点的前一个节点
-			prev.next =  new Node<E>(element, prev.next); //让前一个节点的next指针指向当前新添加的节点
-			//并且让当前新添加节点的next指针,指向前一个节点的所指的next节点
-		} 
+//		if (index == 0) {
+//			first = new Node<E>(element, first);
+//		}
+//		else {
+//			Node<E> prev = node(index - 1);//首先通过index获取当前要添加节点的前一个节点
+//			prev.next =  new Node<E>(element, prev.next); //让前一个节点的next指针指向当前新添加的节点
+//			//并且让当前新添加节点的next指针,指向前一个节点的所指的next节点
+//		} 
 		size ++;
 	}
 
 	@Override
 	public E remove(int index) {
 		rangeCheck(index);
-		Node<E> node = first;
-		if (index == 0) {
-			first = first.next;
-		} else {
-			Node<E> prev = node(index - 1);//首先通过index获取当前要添加节点的前一个节点
-			node = prev.next; //这里node保留了prev.next的节点，即使当prev.next发生改变的时候，node也不会改变，
-//			System.out.println(prev.next);
-//			System.out.println(node);
-			prev.next = node.next;
-//			System.out.println(prev.next);
-//			System.out.println(node);
+		
+		Node<E> node = node(index);
+		Node<E> prev = node.prev;
+		Node<E> next = node.next;
+		
+		if (prev == null) {//index = 0
+			first = next;
+		}else {
+			prev.next = next;
 		}
+		
+		if (next == null) {//index = size -1
+			last = prev;
+		}else {
+			next.prev = prev;
+		}
+		
+//		Node<E> node = first;
+//		if (index == 0) {
+//			first = first.next;
+//		} else {
+//			Node<E> prev = node(index - 1);//首先通过index获取当前要添加节点的前一个节点
+//			node = prev.next; //这里node保留了prev.next的节点，即使当prev.next发生改变的时候，node也不会改变，
+////			System.out.println(prev.next);
+////			System.out.println(node);
+//			prev.next = node.next;
+////			System.out.println(prev.next);
+////			System.out.println(node);
+//		}
 		size --;
 		return node.element;
 	}
@@ -77,21 +140,35 @@ public class LinkedList <E> extends AbstractList<E>{
 			Node<E> node = first;
 			for (int i = 0; i < size; i++) {
 				if (element.equals(node.element)) return i; 
-				
 				node = node.next;
 			}
 		}
 		return ELEMENT_NOT_FOUND;
 	}
-	
+	/*
+	 * 获取index位置对应的节点对象
+	 * @param index
+	 * @return
+	 * */
     
 	private Node<E> node(int index) {
 		rangeCheck(index);
-		Node<E> node = first;
-		for (int i = 0; i < index; i++) {
-			node = node.next;
+		
+		if (index < (size >> 1)) {
+			Node<E> node = first;
+			for (int i = 0; i < index; i++) {
+				node = node.next;
+			}
+			return node;
+		}else {
+			Node<E> node = last;
+			for (int i = size - 1; i > index; i --) {
+				node = node.prev;
+			}
+			return node;
 		}
-		return node;
+		
+		
 	}
 	@Override
 	public String toString() {
--- 03-链表/src/com/sk/Main.java
+++ 03-链表/src/com/sk/Main.java
@@ -1,10 +1,35 @@
 package com.sk;
 
-public class Main {
+import javax.jws.soap.SOAPBinding;
 
-	public static void main(String[] args) {
+public class Main {
+	
+	static void testList(List<Integer> list) { //多态
+		list.add(11);
+		list.add(22);
+		list.add(33);
+		list.add(44);
+
+		list.add(0, 55); // [55, 11, 22, 33, 44]
+		list.add(2, 66); // [55, 11, 66, 22, 33, 44]
+		list.add(list.size(), 77); // [55, 11, 66, 22, 33, 44, 77]
+
+		list.remove(0); // [11, 66, 22, 33, 44, 77]
+		list.remove(2); // [11, 66, 33, 44, 77]
+		list.remove(list.size() - 1); // [11, 66, 33, 44]
+
+		Asserts.test(list.indexOf(44) == 3);
+		Asserts.test(list.indexOf(22) == List.ELEMENT_NOT_FOUND);
+		Asserts.test(list.contains(33));
+		Asserts.test(list.get(0) == 11);
+		Asserts.test(list.get(1) == 66);
+		Asserts.test(list.get(list.size() - 1) == 44);
 		
-		List<Integer> list = new LinkedList<>();
+		System.out.println(list);
+	}
+	
+	static void testList0() {
+		List<Integer> list = new ArrayList<>();
 		list.add(20);
 		System.out.println(list);
 		list.add(0,10);
@@ -17,6 +42,32 @@ public class Main {
 		System.out.println(list);
 		list.set(0, 0);
 		System.out.println(list);
+	}
+	static void josephus() {
+		
+		
+		CircleLinkedList<Integer> list = new CircleLinkedList<Integer>();
+		for (int i = 0; i <= 8; i++) {
+			list.add(i);
+		}
+		
+		//从未想过如此简单 神奇
+		//指向头结点(指向1)
+		list.reset();
+		
+		while (!list.isEmpty()) {
+			list.next();
+			list.next();
+			System.out.println(list.remove());
+			
+		}
+	}
+	public static void main(String[] args) {
+		josephus();
+		testList(new ArrayList<>());
+		testList(new LinkedList<>());
+		testList(new SingleCircleLinkedList<>());
+		testList(new CircleLinkedList<>()); //双向循环链表
 	}
 
 }
